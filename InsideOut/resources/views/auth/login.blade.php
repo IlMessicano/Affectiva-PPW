9751856b04f854c56b9e5c6f475e9340f0c60398
Pierluigi Bemportato <p.bemportato@studenti.uniba.it>
2020-10-01 16:29:36 +0200

    mlml

--- InsideOut/resources/views/auth/login.blade.php
+++ InsideOut/resources/views/auth/login.blade.php
@@ -2,69 +2,51 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <div class="row">
+        <div class="col-md-6">
+            <h2>Login</h2>
+            <form method="post" action=ction="{{ route('login') }}" class="form1">
+                @csrf
+                <div class="avatar-preview" style="background-image: url('{{asset('img/avatar/avatar01.png')}}')">
+                </div>
+                <div style="position:relative; top:-30px;" class="form-group">
+                    <input type="email" name="email" placeholder="Email" class="form-control @error('email') is-invalid @enderror" required value="{{ old('email') }}" autofocus>
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <div style="position:relative; top:-20px;"class="form-group">
+                    <input type="password" name="password" placeholder="Password" class="form-control @error('password') is-invalid @enderror" required autocomplete="current-password">
+                </div>
+                <div class="form-group" style="position:relative; top:-20px;">
+                        <div class="form-check">
+                           <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                          <label class="form-check-label" for="remember">
+                                {{ __('Remember Me') }}
+                            </label>
+                    </div>
+                </div>
+                @if (Route::has('password.request'))
+                    <div class="form-group" style="position:relative; top:-20px;text-align:right;">
+                    <a href="{{ route('password.request') }}">
+                        {{ __('Forgot Your Password?') }}
+                    </a>
+                    </div>
+                @endif
+                <div class="form-group"><input type="submit" class="btn btn-block" value="Accedi"></div>
+                <div class="form-group"><button class="btn btn-block" onclick="{{ route('register') }}">{{ __('Registrati') }}</button></div>
+            </form>
+        </div>
+        <div class="col-md-6">
+            <h2>Live</h2>
+            <div class="live">
+                <div class="row webcam">
+                </div>
+                <div class="row">
+                    <p>Avvia la prova Live</p>
                 </div>
             </div>
         </div>
