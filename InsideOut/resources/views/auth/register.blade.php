b03eac83371cded67796f3a4e32f3975e655be65
Pierluigi Bemportato <p.bemportato@studenti.uniba.it>
2020-10-06 23:42:52 +0200

    Register semifinal

--- InsideOut/resources/views/auth/register.blade.php
+++ InsideOut/resources/views/auth/register.blade.php
@@ -5,18 +5,27 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="avatar-preview-reg" style="background-image: url('{{asset('img/avatar/avatar01.png')}}')">
-            </div>
+
                     <form class="form-reg" method="POST" action="{{ route('register') }}">
                         @csrf
-                        <div class="avatar-preview-reg" style="background-image: url('{{asset('img/avatar/avatar01.png')}}')">
+
+                        <div class="avatar-upload">
+                            <div class="avatar-edit">
+                                <input class="avatar-input" type="file" id="imageUpload" accept=".png, .jpg, jpeg" name="profileimg"/>
+                                <label for="imageUpload" class="fas"></label>
+                            </div>
+                            <div class="avatar-preview-reg">
+                                <div id="imagePreview" style="background-image: url('{{asset('img/avatar/Avatar01.png')}}')"></div>
+                            </div>
+
                         </div>
+
                         <div class="row mb-4"></div>
                         <h2 class="h2">Registrati ora!</h2>
 
                         <div class="form-group">
 
-                            <input id="email" type="email" placeholder="Email*" class="input form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                            <input id="email" type="email" placeholder="Email*" class="input form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                             @error('email')
                             <span class="invalid-feedback" role="alert">
@@ -27,7 +36,7 @@
                         </div>
 
                         <div class="form-group">
-                                <input id="nome" type="text" placeholder="Nome*" class="input form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="nome" type="text" placeholder="Nome*" class="input form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" >
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -37,7 +46,7 @@
                         </div>
 
                         <div class="form-group">
-                            <input id="cognome" type="text" placeholder="Cognome*" class="input form-control @error('cognome') is-invalid @enderror" name="cognome" value="{{ old('cognome') }}" required autocomplete="cognome" autofocus>
+                            <input id="cognome" type="text" placeholder="Cognome*" class="input form-control @error('cognome') is-invalid @enderror" name="cognome" value="{{ old('cognome') }}" required autocomplete="cognome">
 
                             @error('name')
                             <span class="invalid-feedback" role="alert">
@@ -47,13 +56,49 @@
                         </div>
 
                         <div class="form-group">
-                            <input id="data" type="date" placeholder="Cognome*" class="input form-control data @error('cognome') is-invalid @enderror" name="cognome" value="{{ old('cognome') }}" required autocomplete="cognome" autofocus>
+                            <div class="input-group" id="picker" style="cursor:pointer;">
+                                <input placeholder="Data di nascita*" style="cursor:pointer;  background-color:white;" readonly type="text" id="datepicker" class="input form-control data @error('data') is-invalid @enderror" name="data" value="{{ old('data') }}" required autocomplete="data">
+                                <div class="input-group-append">
+                                    <div class="input-group-text btn"><i class="fa fa-calendar" aria-hidden="true"></i></div>
+                                </div>
+                                    <script>
+                                        var picker = new Pikaday({
+                                            field: document.getElementById('datepicker'),
+                                            trigger: document.getElementById('picker'),
+                                            firstDay: 1,
+                                            bound: true,
+                                            i18n: {
+                                                previousMonth : 'Mese precedente',
+                                                nextMonth     : 'Mese successivo',
+                                                months        : ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'],
+                                                weekdays      : ['Domenica','Lunedì','Martedì','Mercoledì','Giovedì','Venerdì','Sabato'],
+                                                weekdaysShort : ['Dom','Lun','Mar','Mer','Gio','Ven','Sab']
+                                            },
+                                            format: 'DD/MM/YYYY',
+                                            toString(date, format) {
+                                                // you should do formatting based on the passed format,
+                                                // but we will just return 'D/M/YYYY' for simplicity
+                                                const day = date.getDate();
+                                                const month = date.getMonth() + 1;
+                                                const year = date.getFullYear();
+                                                return `${day}/${month}/${year}`;
+                                            },
+                                            position: 'top right',
+                                            yearRange: [1940, moment().get('year')],
+                                            minDate: new Date(1920, 0, 1),
+                                            maxDate: moment().toDate()
 
-                            @error('name')
+                                        });
+
+                                    </script>
+                            </div>
+
+                            @error('data')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
                             @enderror
+
                         </div>
 
                         <div class="form-group">
@@ -78,6 +123,7 @@
                                 </button>
                         </div>
                     </form>
+
         </div>
     </div>
 </div>
--- InsideOut/resources/views/layouts/app.blade.php
+++ InsideOut/resources/views/layouts/app.blade.php
@@ -12,15 +12,18 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/pikaday/pikaday.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.0/moment.min.js" integrity="sha512-Izh34nqeeR7/nwthfeE0SI3c8uhFSnqxV0sI9TvTcXiFJkMd6fB644O64BRq2P/LA/+7eRvCw4GmLsXksyTHBg==" crossorigin="anonymous"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
     <link href="{{ asset('css/login_register.css') }}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/pikaday/css/pikaday.css">
 
 </head>
 <body>
@@ -38,7 +41,7 @@
                     </div>
                         @if (Route::has('register'))
                         <div class="col-1 links">
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}">Registrati</a>
                         </div>
                         @endif
                     @endauth
